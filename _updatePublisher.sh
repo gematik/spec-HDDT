7e0fee6f0eb79b66fecd2abb53622edb9e19ccc0
Emil Milanov <emil.milanov@fbeta.de>
2026-03-04 13:56:28 +0100

    Bump ig-publisher version

--- _updatePublisher.sh
+++ _updatePublisher.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pubsource=https://github.com/HL7/fhir-ig-publisher/releases/download/2.1.0/
+pubsource=https://github.com/HL7/fhir-ig-publisher/releases/download/2.1.2/
 publisher_jar=publisher.jar
 dlurl=$pubsource$publisher_jar
 
